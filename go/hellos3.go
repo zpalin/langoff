528d9f32161fce5493763c2f2143c95226f0b027
Zac Palin <zpalin@gmail.com>
2020-04-02 12:05:31 -0700

    Adds GO S3 example

---
+++ go/hellos3.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+func main() {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	svc := s3.New(sess)
+
+	resp, err := svc.ListBuckets(nil); if err != nil {
+		fmt.Printf("Error: %+v\n", err)
+		return
+	}
+
+	bucket := resp.Buckets[0]
+
+	input := s3.ListObjectsInput{
+		Bucket: bucket.Name,
+	}
+	objResp, err := svc.ListObjects(&input); if err != nil {
+		fmt.Printf("Error: %+v\n", err)
+		return
+	}
+
+	fmt.Printf("%+v\n", objResp)
+}
